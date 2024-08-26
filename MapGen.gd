66a11d2b2e63c877a1f80674c5ad615b3d518f4e
Tero Hongisto <tehongis@gmail.com>
2024-08-26 22:40:30 +0300

    Update MapGen.gd

--- MapGen.gd
+++ MapGen.gd
@@ -12,15 +12,15 @@ func _ready() -> void:
 	upart.seed= randi()
 	vpart.seed= randi()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	generate_chunk(player.position)
 
 func generate_chunk(position):
 	var tile_pos = local_to_map(position)
 	for x in range(width):
 		for y in range(height):
-			var u = 10+upart.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2)*10
-			var v = 8+vpart.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2)*8
+			var u = 17+round(upart.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2)*2)
+			var v = round(vpart.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2)*2)
 			set_cell(Vector2i(tile_pos.x - width/2 + x, tile_pos.y - height/2 + y),0,Vector2i(u,v))
 			#set_cell(Vector2i(tile_pos.x + x - width/2, tile_pos.y + y - height/2),0,Vector2i(0,0))
 		
