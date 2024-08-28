ac68360711e2647f4fa20e69e0fad5e9512ac899
Tero Hongisto <tehongis@gmail.com>
2024-08-28 09:34:34 +0300

    movement updates

--- MapGen.gd
+++ MapGen.gd
@@ -1,7 +1,8 @@
 extends TileMapLayer
 
-var upart = FastNoiseLite.new()
-var vpart = FastNoiseLite.new()
+var noise = FastNoiseLite.new()
+#var upart = FastNoiseLite.new()
+#var vpart = FastNoiseLite.new()
 
 var width = 128
 var height = 64
@@ -9,8 +10,9 @@ var height = 64
 @onready var player = get_parent().get_node("Player")
 
 func _ready() -> void:
-	upart.seed= randi()
-	vpart.seed= randi()
+	noise.seed= 3
+	#upart.seed= randi()
+	#vpart.seed= randi()
 
 func _process(_delta: float) -> void:
 	generate_chunk(player.position)
@@ -19,9 +21,16 @@ func generate_chunk(position):
 	var tile_pos = local_to_map(position)
 	for x in range(width):
 		for y in range(height):
-			var u = 17+round(upart.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2)*2)
-			var v = round(vpart.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2)*2)
-			set_cell(Vector2i(tile_pos.x - width/2 + x, tile_pos.y - height/2 + y),0,Vector2i(u,v))
+			var tileid = abs( round( 2*noise.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2) ) )
+			#print(tileid)
+			#var u = 17+round(upart.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2)*2)
+			#var v = round(vpart.get_noise_2d(tile_pos.x + x - width/2, tile_pos.y + y - height/2)*2)
+			if tileid == 1:
+				set_cell(Vector2i(tile_pos.x - width/2 + x, tile_pos.y - height/2 + y),0,Vector2i(2,6))
+			#else:
+			#	set_cell(Vector2i(tile_pos.x - width/2 + x, tile_pos.y - height/2 + y),0,Vector2i(13,3))
+
+			#set_cell(Vector2i(tile_pos.x - width/2 + x, tile_pos.y - height/2 + y),0,Vector2i(u,v))
 			#set_cell(Vector2i(tile_pos.x + x - width/2, tile_pos.y + y - height/2),0,Vector2i(0,0))
 		
 	
--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,16 +1,31 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 20
 @export var rotation_speed = 2
+@export var inertia = 0.97
+@export var gravity = 4
 
 var rotation_direction = 0
 
 func get_input():
 	#rotation = get_global_mouse_position().angle_to_point(position)
-	rotation_direction = Input.get_axis("move_left", "move_right")
-	velocity = transform.x * Input.get_axis("move_down", "move_up") * speed
+	#rotation_direction = Input.get_axis("move_left", "move_right")
+	velocity.x += Input.get_axis("move_left", "move_right") * speed
+	velocity.y += Input.get_axis("move_up", "move_down") * speed
 
 func _physics_process(delta):
 	get_input()
-	rotation += rotation_direction * rotation_speed * delta
+	#rotation += rotation_direction * rotation_speed * delta
+
+#	velocity.x = lerp(velocity.x, 0, 0.02)
+#	velocity.y = lerp(velocity.y, 0, 0.02)
+
+	if is_on_floor():
+		velocity.x = 0
+
+	velocity.x = velocity.x * inertia
+	velocity.y = velocity.y * inertia
+
+	velocity.y += gravity
+
 	move_and_slide()
